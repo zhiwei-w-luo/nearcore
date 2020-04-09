9b146cc3b23ee3ea6bdd1d57fb53c5861169c974
Marcelo Fornet <mfornet94@gmail.com>
2020-04-08 23:35:57 -0400

    Increase MAX_BLOCK_PRODUCTION_DELAY to 6 seconds.

--- neard/src/config.rs
+++ neard/src/config.rs
@@ -52,8 +52,8 @@ pub const BLOCK_PRODUCTION_TRACKING_DELAY: u64 = 100;
 /// Expected block production time in ms.
 pub const MIN_BLOCK_PRODUCTION_DELAY: u64 = 600;
 
-/// Maximum time to delay block production without approvals is ms.
-pub const MAX_BLOCK_PRODUCTION_DELAY: u64 = 2_000;
+/// Maximum time to delay block production without approvals in ms.
+pub const MAX_BLOCK_PRODUCTION_DELAY: u64 = 6_000;
 
 /// Maximum time until skipping the previous block is ms.
 pub const MAX_BLOCK_WAIT_DELAY: u64 = 6_000;
